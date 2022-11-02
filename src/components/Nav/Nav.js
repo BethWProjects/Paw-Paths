323850b5e5191ca800ff51fcfbc8c264c0e52a66
Beth Wilson <bethwilson777@gmail.com>
2022-11-02 15:59:03 -0600

    Added Nav and CSS updates

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css'
+import pawLogo from '../../pawLogo.png'
+
+const Nav = () => {
+    return (
+        <div className='nav-container'>
+            <img className="pawLogo" src={ pawLogo } alt="tree and path logo"/>
+            <div className='title-div'>
+            <h1 className='title'>Paw Paths</h1>
+            <p className='tagline'>For the walks you haven't taken yet...</p>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
