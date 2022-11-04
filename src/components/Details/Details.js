f2bd94f368e30763a0bc4795f5a96f5134a20e1e
Beth Wilson <bethwilson777@gmail.com>
2022-11-04 16:59:27 -0600

    Updated for accessibility

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -31,13 +31,13 @@ class Details extends Component {
     const path = this.state.path
     return (
       <div className='path-details-container'>
-        <img src={path.image} className='details-image' />
+        <img src={path.image} className='details-image' alt={`image of ${path.title}`} />
         <div className='content-details-container'>
           <h2 className='title'> {path.title} </h2>
-          <h3 className='location'><img src={location} className='icon'/> Location - {path.location}</h3>
-          <h3 className='path-details'> <img src={footprints} className='icon'/> Distance - {path.length} </h3>
-          <h3 className='path-details'><img src={difficulty} className='icon'/> Difficulty - {path.difficulty}</h3>
-          <h3 className='path-details'><img src={leash} className='icon'/> Leash required? - {path.leash}</h3>
+          <h3 className='location'><img src={location} className='icon' alt="Location Icon"/> Location - {path.location}</h3>
+          <h3 className='path-details'> <img src={footprints} className='icon' alt="Distance Footprint Icon"/> Distance - {path.length} </h3>
+          <h3 className='path-details'><img src={difficulty} className='icon' alt="Difficulty Spicy Pepper Icon"/> Difficulty - {path.difficulty}</h3>
+          <h3 className='path-details'><img src={leash} className='icon' alt="Dog with Leash Required Icon"/> Leash required? - {path.leash}</h3>
           <h3 className="overview"> {path.overview} </h3>  
         <Link to={'/'}>
           <button className='details-home-btn'>Home</button>
